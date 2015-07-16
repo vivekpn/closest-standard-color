2583cc4be3a26bd5fbafdfb3eb3f65d831097d08
unknown <vivekpn@users.noreply.github.com>
2015-07-17 01:51:40 +0530

    Reformated the code. Deleted the duplicated file

--- colorVariant.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by vivekp on 17-07-2015.
- */
--- js/colorVariant.js
+++ js/colorVariant.js
@@ -1,32 +1,33 @@
 "use strict";
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.color').colpick({
-        colorScheme:'dark',
-        layout:'rgbhex',
-        color:'ff8800',
-        onSubmit:function(hsb,hex,rgb,el) {
-            $(el).css('background-color', '#'+hex);
+        colorScheme: 'dark',
+        layout: 'rgbhex',
+        color: 'ff8800',
+        onSubmit: function (hsb, hex, rgb, el) {
+            $(el).css('background-color', '#' + hex);
             $(el).colpickHide();
             calculateColorVariationAndUpdateStatus();
-        }});
+        }
+    });
     calculateColorVariationAndUpdateStatus();
 });
 
 var calculateColorVariationAndUpdateStatus = function () {
-    var result = calculateColorVariation($('#left-picker').css('background-color'),$('#right-picker').css('background-color'));
+    var result = calculateColorVariation($('#left-picker').css('background-color'), $('#right-picker').css('background-color'));
     updateStatusDivision(result);
 };
 
-var calculateColorVariation = function(rgb1,rgb2){
-    var left_color= parseRgb(rgb1);
+var calculateColorVariation = function (rgb1, rgb2) {
+    var left_color = parseRgb(rgb1);
     var right_color = parseRgb(rgb2);
-    return findColorDifference(left_color,right_color);
+    return findColorDifference(left_color, right_color);
 };
 
-var parseRgb = function(rgb){
+var parseRgb = function (rgb) {
     var colorsOnly = rgb.substring(rgb.indexOf('(') + 1, rgb.lastIndexOf(')')).split(/,\s*/);
-    var  components = {};
+    var components = {};
     components.red = colorsOnly[0];
     components.green = colorsOnly[1];
     components.blue = colorsOnly[2];
@@ -34,15 +35,15 @@ var parseRgb = function(rgb){
     return components;
 };
 
-var updateStatusDivision = function(status){
+var updateStatusDivision = function (status) {
     $('#color_difference').text(status);
 };
 
-var findColorDifference = function(color1,color2){
+var findColorDifference = function (color1, color2) {
     var sumOfsquaresOfDifferences = 0;
     var SCALING_CONSTANT = 2.2641187;
-    sumOfsquaresOfDifferences+= Math.pow((color1.red - color2.red),2);
-    sumOfsquaresOfDifferences+= Math.pow((color1.green - color2.green),2);
-    sumOfsquaresOfDifferences+= Math.pow((color1.blue - color2.blue),2);
-    return Math.round(Math.sqrt(sumOfsquaresOfDifferences)* SCALING_CONSTANT);
+    sumOfsquaresOfDifferences += Math.pow((color1.red - color2.red), 2);
+    sumOfsquaresOfDifferences += Math.pow((color1.green - color2.green), 2);
+    sumOfsquaresOfDifferences += Math.pow((color1.blue - color2.blue), 2);
+    return Math.round(Math.sqrt(sumOfsquaresOfDifferences) * SCALING_CONSTANT);
 };
